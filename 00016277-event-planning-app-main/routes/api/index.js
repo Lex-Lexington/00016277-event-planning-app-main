307a84791cf3938f4b33cfee5e7472bc1d00448d
Aleksandr Bukminov <136957620+Lex-Lexington@users.noreply.github.com>
2026-03-08 22:03:49 +0500

    Delete 00016277-event-planning-app-main directory

--- 00016277-event-planning-app-main/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const event_router = require('./event')
-
-const router = express.Router()
-
-// registering child routers
-router.use('/event', event_router)
-module.exports = router
